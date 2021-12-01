de4e763f7e3db66be741a2ef32ec1bbd1fdf45b9
Yuxuan Gao <jasongao@usc.edu>
2021-11-30 23:00:52 -0800

    update hand and game

--- src/main/java/com/example/pokersc/entity/GameThread.java
+++ src/main/java/com/example/pokersc/entity/GameThread.java
@@ -3,32 +3,26 @@ package com.example.pokersc.entity;
 public class GameThread extends Thread{
 
     private Game game;
-    private Action currentAction = null;
 
     public GameThread(){
         this.game = new Game();
     }
 
     public void run(){
-        while(true) {
-            if(game.numPlayers < 3){
-                try {
+        try {
+            while (true) {
+                if (game.numPlayers < 3) {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } else {
+                    game.updatePos();
+                    Hand hand = new Hand(game.userArr, game.dealerPos, game.numPlayers);
+                    hand.startHand();
                 }
             }
-            else {
-                game.updatePos();
-                Hand hand = new Hand(game.userArr, game.dealerPos, game.numPlayers);
-                hand.startHand();
-                while(this.currentAction == null){}
-            }
         }
-    }
-
-    public void addAction(Action action) {
-        this.currentAction = action;
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void main(){
--- src/main/java/com/example/pokersc/entity/Hand.java
+++ src/main/java/com/example/pokersc/entity/Hand.java
@@ -10,17 +10,32 @@ public class Hand {
     private int dealerPos;
     private int numPlayers;
     private PlayerCards[] playerCards;
+    private boolean[] active;
+    private int[] betInThisPhase;
+    private int pot;
     private Deck deck;
     private Card[] flop;
     private Card turn;
     private Card river;
     private int actionOnWhichPlayer;
+    private Action currentAction = null;
 
     public Hand(User[] userList, int dPos, int numP){
         this.playerArr = userList;
         this.dealerPos = dPos;
         this.numPlayers = numP;
         this.playerCards = new PlayerCards[8];
+        this.active = new boolean[8];
+        this.pot = 0;
+        for(int i = 0; i < 8; i++){
+            betInThisPhase[i] = 0;
+            if(playerArr[i] != null) {
+                active[i] = true;
+            }
+            else {
+                active[i] = false;
+            }
+        }
         this.deck = new Deck();
         this.flop = new Card[3];
         actionOnWhichPlayer = dealerPos + 3; //first action on UTG;
@@ -41,10 +56,20 @@ public class Hand {
         //flop
         int numActionLeft = numPlayers;
         while(numActionLeft > 0){
-
+            while (true){
+                if(currentAction != null){
+                    break;
+                }
+            }
+            doAction();
+            this.currentAction = null;
         }
+
     }
 
+    public void doAction(){
+
+    }
     // deal cards given the position of button
     private void dealCardsToPlayers(int buttonPos) {
         int start = (buttonPos + 2) % 8;
@@ -75,6 +100,18 @@ public class Hand {
         river = this.deck.dealCard();
     }
 
+    public void addAction(Action action) {
+        try {
+            while (currentAction != null) {
+                Thread.sleep(1000);
+            }
+            this.currentAction = action;
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     // Above is written by Jason
     // Below is written by Peter
     public void startUserThread() {
@@ -85,6 +122,8 @@ public class Hand {
         exec.shutdown();
         while(!exec.isTerminated()){Thread.yield();}
     }
+
+
 
     public Action getRecentAction() {
         return new Action(playerArr[0], Action.Act.CHECK);
