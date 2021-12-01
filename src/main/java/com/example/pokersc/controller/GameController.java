bcbc68e0b13fc8f63c1258da4e54673c579867e7
Yutong Li <yli81711@usc.edu>
2021-11-30 20:20:30 -0800

    modify user table structure

--- src/main/java/com/example/pokersc/controller/GameController.java
+++ src/main/java/com/example/pokersc/controller/GameController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(name ="/api")
 public class GameController {
 
     @Autowired
@@ -13,52 +14,46 @@ public class GameController {
     private Game game;
 
     // a user creates a game
-    @PostMapping("games/")
+    @PostMapping("/games")
     public Game createGame() {
         //TODO join after creating game
         return null;
     }
 
-    @GetMapping("games/{game_id}/")
-    public Game getGameById(@PathVariable int game_id) {
-        //TODO
-        return null;
-    }
-
-    @PostMapping("games/{game_id}/{user_id}")
-    public Game joinGameById(@PathVariable int game_id, @PathVariable int user_id) {
+    @PostMapping("/games/{user_id}")
+    public Game joinGameById(@PathVariable int user_id, @RequestParam int position, @RequestParam int buyin) {
         //TODO
         // call something like addUser();
         return null;
     }
 
-    @PostMapping("games/{game_id}/{user_id}/leave")
-    public Game leaveGameById(@PathVariable int game_id, @PathVariable int user_id) {
+    @PostMapping("/games/{user_id}/leave")
+    public Game leaveGameById(@PathVariable int user_id) {
         //TODO
         // call something like deleteUser();
         return null;
     }
 
-    @PostMapping("games/{game_id}/{user_id}/fold")
-    public Game userFold(@PathVariable int game_id, @PathVariable int user_id) {
+    @PostMapping("/games/{user_id}/fold")
+    public Game userFold(@PathVariable int user_id) {
         //TODO
         return null;
     }
 
-    @PostMapping("games/{game_id}/{user_id}/check")
-    public Game userCheck(@PathVariable int game_id, @PathVariable int user_id) {
+    @PostMapping("/games/{user_id}/check")
+    public Game userCheck(@PathVariable int user_id) {
         //TODO
         return null;
     }
 
-    @PostMapping("games/{game_id}/{user_id}/call")
-    public GameController userCall(@PathVariable int game_id, @PathVariable int user_id, @RequestParam int amount) {
+    @PostMapping("/games/{user_id}/call")
+    public GameController userCall( @PathVariable int user_id, @RequestParam int amount) {
         //TODO
         return null;
     }
 
-    @PostMapping("games/{game_id}/{user_id}/raise")
-    public GameController userRaise(@PathVariable int game_id, @PathVariable int user_id, @RequestParam int amount) {
+    @PostMapping("games/{user_id}/raise")
+    public GameController userRaise(@PathVariable int user_id, @RequestParam int amount) {
         //TODO
         return null;
     }
--- src/main/java/com/example/pokersc/controller/UserController.java
+++ src/main/java/com/example/pokersc/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-//@RequestMapping(name ="/api")
+@RequestMapping(name ="/api")
 public class UserController {
 
     @Autowired
