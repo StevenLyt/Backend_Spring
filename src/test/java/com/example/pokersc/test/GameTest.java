0d3729aa995cb1cd3423bc27be57b8eeb41410a6
Thomas Liang <thmsljj@outlook.com>
2021-12-05 21:52:42 -0800

    add a GameTest.java

---
+++ src/test/java/com/example/pokersc/test/GameTest.java
@@ -0,0 +1,40 @@
+package com.example.pokersc.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void rebuy() {
+    }
+
+    @Test
+    void updatePos() {
+    }
+
+    @Test
+    void updateUserStats() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
